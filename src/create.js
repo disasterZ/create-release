003154c3ece19ec485d781e5965f36dba4906ce1
disaseterZ <937934696@qq.com>
2022-07-30 15:00:25 +0800

    output test

---
+++ src/create.js
@@ -0,0 +1,21 @@
+import core from '@actions/core';
+import { GitHub, context } from '@actions/github';
+import fs from 'fs';
+
+const run = async () => {
+  try {
+    // get authenticated GitHub client
+    const github = new GitHub(process.env.GITHUB_TOKEN);
+    core.setOutput('out', github);
+    console.log(github);
+
+    // get onwer and repo
+    const { owner: currentOwner, repo: currentRepo } = context.repo;
+
+    //
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+export default run;
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import run from './create';
+
+if (require.main === module) {
+  run();
+}
